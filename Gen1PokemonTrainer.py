cb94068a88dedb7f94b022e44160323894a4da07
Andrew Reed <stabr02@moravian.edu>
2017-06-23 00:12:39 -0400

    Commit 2: Added function to get Pokemon's number
    Refactors: extracted buildQuestion method

--- Gen1PokemonTrainer.py
+++ Gen1PokemonTrainer.py
@@ -1,18 +1,47 @@
 import logging
 from flask import Flask
 from flask_ask import Ask, statement, question, session
+import mysql.connector
 
 app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
+#pokemonSchema = (name, nationalIDNumber, type1, type2, classification, height, weight, captureRate, evolvedFrom, baseHP, baseAttack, baseDefense, baseSpecial, baseSpeed)
+#evolveToSchema = (pokemon, pokemonEvolution, evolutionLevel, otherEvolutionMethod)
+#pokedexEntrySchema = (pokemon, game, entry)
+#pokemonLocationSchema = (pokemon, game, location)
+#resistsSchema = (userType1, userType2, enemyMoveType, multiplier)
+#weakToSchema = (userType1, userType2, enemyMoveType, multiplier)
+#movesSchema = (name, type, PP, basePower, accuracy, battleEffect, effectRate, TMNumber, HMNumber, speedPriority, pokemonHitInBattle)
+#movesFromLevelingSchema = (pokemon, move, level)
+#movesFromMachineSchema = (pokemon, move)
+
 @ask.launch
 def launchAppWithoutIntent():
-    return question("Ask me a question about gen 1 Pokemon.").reprompt("Ask me a question about gen 1 Pokemon.")
+    return buildQuestion()
 
 @ask.session_ended
 def closeApp():
     return "{}", 200
 
-print(launchAppWithoutIntent()._response)
-print(closeApp())
+@ask.intent('PokemonNumberIntent')
+def getPokemonNumber(pokemon):
+
+    cnx = mysql.connector.connect(user='nottelling', password='nicetry;)',
+                                  host='gen1pokemonapp.cacuxkbfp0fa.us-east-2.rds.amazonaws.com',
+                                  database='Gen1PokemonApp')
+
+    cursor = cnx.cursor()
+    query = ("SELECT * from Pokemon WHERE Name = '{}'".format(pokemon))
+    cursor.execute(query)
+    for (name, nationalIDNumber, type1, type2, classification, height, weight, captureRate, evolvedFrom, baseHP,
+         baseAttack, baseDefense, baseSpecial, baseSpeed) in cursor:
+
+        return buildQuestion("{} has the ID number {}. ".format(name, nationalIDNumber))
+
+    return buildQuestion("{} isn't a Pokemon. ".format(pokemon))
+
+def buildQuestion(alexaResponse = ""):
+    return question(alexaResponse + "Ask me a question about gen 1 Pokemon.")\
+        .reprompt("Ask me a question about gen 1 Pokemon.")
--- Tests/TestAppLaunchWithoutIntent.py
+++ Tests/TestAppLaunchWithoutIntent.py
@@ -4,8 +4,25 @@ from Gen1PokemonTrainer import *
 
 class TestAppLaunchWithoutIntent(TestCase):
     def testLaunchApp(self):
-        self.assertEquals(launchAppWithoutIntent()._response, question("Ask me a question about gen 1 Pokemon.")
-                          .reprompt("Ask me a question about gen 1 Pokemon.")._response)
+        self.assertEquals(launchAppWithoutIntent()._response, buildQuestion()._response)
 
     def testCloseApp(self):
         self.assertEquals(closeApp(), ('{}', 200))
+
+    def testGetPokemonNumberPass(self):
+        self.assertEquals(getPokemonNumber("bulbasaur")._response,
+                          buildQuestion("Bulbasaur has the ID number 1. ")._response)
+
+    def testGetPokemonNumberFail(self):
+        self.assertEquals(getPokemonNumber("bob")._response,
+                          buildQuestion("bob isn't a Pokemon. ")._response)
+
+    def testBuildQuestionEmpty(self):
+        self.assertEquals(buildQuestion()._response,
+                          question("Ask me a question about gen 1 Pokemon.")
+                          .reprompt("Ask me a question about gen 1 Pokemon.")._response)
+
+    def testBuildQuestion(self):
+        self.assertEquals(buildQuestion("yo dude, what's up?")._response,
+                          question("yo dude, what's up?Ask me a question about gen 1 Pokemon.")
+                          .reprompt("Ask me a question about gen 1 Pokemon.")._response)
